3593200e4b1a522832b14b84caa95dcb7c61da97
fangmingdong <fangmingdong0224@gmail.com>
2021-01-02 10:23:59 +0800

    fix dockerfile

--- start-docker.sh
+++ start-docker.sh
@@ -8,9 +8,9 @@ git pull &&
 
 echo "start docker"
 
-docker build -t double-official . &&
+docker build -t double-official1 . &&
 
-container=$(docker container ls -aqf "name=double-official")
+container=$(docker container ls -aqf "name=double-official1")
 
 if [ -n "$container" ]; then
     echo "stop container and rm container"
@@ -18,8 +18,8 @@ if [ -n "$container" ]; then
     docker container rm $container
 
     echo "start container"
-    docker run --name double-official -d -p  3020:3020 double-official
+    docker run --name double-official1 -d -p  3020:3020 double-official1
 else
     echo "start container"
-    docker run --name double-official -d -p  3020:3020 double-official  
+    docker run --name double-official1 -d -p  3020:3020 double-official1  
 fi
