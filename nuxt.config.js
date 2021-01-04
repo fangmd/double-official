d81ce881389d300a6f0d23d01f3edc9ee639427b
fangmingdong <fangmingdong0224@gmail.com>
2021-01-04 11:08:40 +0800

    fix: seo baidu 搜索

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,16 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    terser: {
+      // https://github.com/terser/terser#compress-options
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    },
+  },
   server: {
     port: 3020,
     host: '0.0.0.0',
