d1df8e82cd2890caeb1df6fcbf2c3d196090143e
fangmingdong <fangmingdong0224@gmail.com>
2021-01-05 16:07:11 +0800

    feat: rm bootstrapvue

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,35 @@ export default {
       { hid: 'description', name: 'description', content: 'Double' },
       { name: 'keywords', content: 'fangmingdong, blog' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href:
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
+        rel: 'stylesheet',
+        integrity:
+          'sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1',
+        crossorigin: 'anonymous',
+      },
+    ],
     script: [
       {
         src: '/baidu.js',
       },
+      // {
+      //   src: 'bootstrap/dist/js/bootstrap.bundle.min.js',
+      //   body: true,
+      // },
+
+      {
+        src:
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW',
+        crossorigin: 'anonymous',
+        body: true,
+      },
+
       // {
       //   src: 'https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js',
       //   body: true,
@@ -29,7 +53,11 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/css/base.less', 'github-markdown-css/github-markdown.css'],
+  css: [
+    '~/assets/css/base.less',
+    'github-markdown-css/github-markdown.css',
+    // 'bootstrap/dist/css/bootstrap.min.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['~/plugins/axios', '~plugins/filters.js'],
@@ -45,8 +73,6 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
