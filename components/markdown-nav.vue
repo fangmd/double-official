4bb913057ec1ac44cb735f8063129dbeab72dc49
fangmingdong <fangmingdong0224@gmail.com>
2021-01-05 11:07:55 +0800

    feat: article nav

---
+++ components/markdown-nav.vue
@@ -0,0 +1,122 @@
+/* eslint-disable vue/no-v-html */
+<template>
+  <div class="markdown-nav">
+    <ul>
+      <li
+        v-for="(item, index) in getContent"
+        :key="index"
+        :class="item.type"
+        class="nav-item"
+      >
+        <a :href="`#${item.id}`">{{ item.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { md2html } from '@/utils/md-utils'
+
+export default {
+  name: 'MarkdownNav',
+  props: {
+    mdContent: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    getContent() {
+      const htmlContent = md2html(this.mdContent)
+      const lines = htmlContent.split('\n')
+      const nodeRoot = []
+      for (const l of lines) {
+        if (l.startsWith('<h1')) {
+          const node = this.getNode(l)
+          node.type = 'h1'
+          nodeRoot.push(node)
+        }
+        if (l.startsWith('<h2')) {
+          const node = this.getNode(l)
+          node.type = 'h2'
+          nodeRoot.push(node)
+        }
+        if (l.startsWith('<h3')) {
+          const node = this.getNode(l)
+          node.type = 'h3'
+          nodeRoot.push(node)
+        }
+        if (l.startsWith('<h4')) {
+          const node = this.getNode(l)
+          node.type = 'h4'
+          nodeRoot.push(node)
+        }
+        if (l.startsWith('<h5')) {
+          const node = this.getNode(l)
+          node.type = 'h5'
+          nodeRoot.push(node)
+        }
+      }
+      return nodeRoot
+    },
+  },
+  methods: {
+    getNode(l) {
+      const node = {}
+      node.title = this.getTitle(l)
+      node.id = this.getTagContent(l)
+      return node
+    },
+    getTitle(l) {
+      return l.substring(l.indexOf('>') + 1, l.lastIndexOf('<'))
+    },
+    getTagContent(l) {
+      return l.substring(l.indexOf('=') + 1, l.indexOf('>')).replaceAll('"', '')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.markdown-nav {
+  position: fixed;
+  top: 5vh;
+  right: 0;
+  height: 90vh;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  ul {
+    height: 100%;
+  }
+
+  .nav-item {
+    a {
+      color: #232629;
+    }
+  }
+
+  .h1 {
+    font-size: 1.3em;
+  }
+  .h2 {
+    font-size: 0.97em;
+  }
+  .h3 {
+    font-size: 0.9em;
+  }
+  .h4 {
+    font-size: 0.8em;
+  }
+  .h5 {
+    font-size: 0.6em;
+  }
+}
+@media (max-width: 767.98px) {
+  .markdown-nav {
+    display: none;
+  }
+}
+</style>
--- pages/article-detail.vue
+++ pages/article-detail.vue
@@ -1,37 +1,44 @@
 /* eslint-disable vue/no-v-html */
 <template>
   <div class="page-root container-lg">
-    <div class="article-header">
-      <p class="title">{{ article.title }}</p>
-      <p class="time">
-        创建时间：{{ article.createTime | formateServerTime }} 更新时间：{{
-          article.updateTime | formateServerTime
-        }}
-      </p>
+    <div class="row">
+      <div class="col-md-2 col-0"></div>
+      <div class="col-md-8 col-12">
+        <div class="article-header">
+          <p class="title">{{ article.title }}</p>
+          <p class="time">
+            创建时间：{{ article.createTime | formateServerTime }} 更新时间：{{
+              article.updateTime | formateServerTime
+            }}
+          </p>
+        </div>
+        <div class="tags">
+          <span
+            v-for="(item, index) in article.tagsArr"
+            :key="index"
+            class="badge bg-light text-dark home-tag"
+            >{{ item }}</span
+          >
+        </div>
+        <markdown-content :md-content="article.content"></markdown-content>
+        <div id="SOHUCS" :sid="article.id"></div>
+      </div>
+      <markdown-nav :md-content="article.content" class="col-md-2 col-0" />
     </div>
-    <div class="tags">
-      <span
-        v-for="(item, index) in article.tagsArr"
-        :key="index"
-        class="badge bg-light text-dark home-tag"
-        >{{ item }}</span
-      >
-    </div>
-    <markdown-content :md-content="article.content"></markdown-content>
-    <div id="SOHUCS" :sid="article.id"></div>
   </div>
 </template>
 
 <script>
 import { getArticle } from '@/apis/blog'
 import MarkdownContent from '@/components/markdown-content'
+import MarkdownNav from '@/components/markdown-nav'
 
 /**
  * 文章详情页
  */
 export default {
   name: 'ArticleDetail',
-  components: { MarkdownContent },
+  components: { MarkdownContent, MarkdownNav },
   async asyncData({ route, $axios }) {
     const { id } = route.query
     const ret = await getArticle($axios, { id })
