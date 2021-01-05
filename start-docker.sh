0e3940155e05887a47d85204bd23c927e50173a5
fangmingdong <fangmingdong0224@gmail.com>
2021-01-05 11:21:15 +0800

    feat: sh

--- start-docker.sh
+++ start-docker.sh
@@ -10,7 +10,7 @@ echo "start docker"
 
 docker build -t double-official1 . &&
 
-container=$(docker container ls -aqf "name=double-official1")
+container=$(docker container ls -aqf "name=double-official-c")
 
 if [ -n "$container" ]; then
     echo "stop container and rm container"
@@ -18,8 +18,8 @@ if [ -n "$container" ]; then
     docker container rm $container &&
 
     echo "start container"
-    docker run --name double-official1 -d -p  3020:3020 double-official1
+    docker run --name double-official-c -d -p  3020:3020 double-official1
 else
     echo "start container"
-    docker run --name double-official1 -d -p  3020:3020 double-official1  
+    docker run --name double-official-c -d -p  3020:3020 double-official1  
 fi
